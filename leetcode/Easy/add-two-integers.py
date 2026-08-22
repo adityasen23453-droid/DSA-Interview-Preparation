baff1d018c3e1bb2d667447774e1a3ee3c0fd30f
Aditya Sen <adityasen23453@gmail.com>
2026-08-22 11:22:35 +0530

    Solved: Add Two Integers (leetcode)

---
+++ leetcode/Easy/add-two-integers.py
@@ -0,0 +1,13 @@
+// Problem: Add Two Integers
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: python
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/add-two-integers/
+// Solved on: 2026-08-22T05:52:22.956Z
+
+class Solution(object):
+    def sum(self, num1, num2):
+     object = num1 + num2
+     return object
+        

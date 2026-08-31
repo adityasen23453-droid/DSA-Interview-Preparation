022be547b49b19114f34c237698dedf03fdd939e
Aditya Sen <adityasen23453@gmail.com>
2026-08-31 23:45:39 +0530

    Solved: Contains Duplicate II (leetcode)

---
+++ leetcode/Easy/contains-duplicate-ii.py
@@ -0,0 +1,18 @@
+// Problem: Contains Duplicate II
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: python
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/contains-duplicate-ii/
+// Solved on: 2026-08-31T18:15:33.609Z
+
+class Solution():
+   def containsNearbyDuplicate(self , nums , k):
+       window = set()
+       for i in range(len(nums)):
+           if nums[i] in window:
+              return True
+           window.add(nums[i])
+           if len(window)>k:
+              window.remove(nums[i-k]) 
+       return False

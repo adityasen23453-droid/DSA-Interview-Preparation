42d80c94a5b1f16c4486e2239b059167f0a3298b
Aditya Sen <adityasen23453@gmail.com>
2026-09-02 20:38:18 +0530

    Solved: Check if Array Is Sorted and Rotated (leetcode)

---
+++ leetcode/Easy/check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,15 @@
+// Problem: Check if Array Is Sorted and Rotated
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: python
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+// Solved on: 2026-09-02T15:08:10.668Z
+
+class Solution:
+   def check(self , nums) :
+       count = 0
+       for i in range(len(nums)):
+          if nums[i] > nums[(i + 1) % len(nums)]:
+                 count += 1
+       return count <= 1

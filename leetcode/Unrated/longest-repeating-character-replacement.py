f0bc58dfd55bf3b29f112ad5ea794d6b3152ded3
Aditya Sen <adityasen23453@gmail.com>
2026-09-14 10:48:56 +0530

    Solved: Longest Repeating Character Replacement (leetcode)

--- leetcode/Unrated/longest-repeating-character-replacement.py
+++ leetcode/Unrated/longest-repeating-character-replacement.py
@@ -4,7 +4,7 @@
 // Language: python
 // Verdict: Accepted
 // URL: https://leetcode.com/problems/longest-repeating-character-replacement/
-// Solved on: 2026-09-10T13:46:51.650Z
+// Solved on: 2026-09-14T05:18:43.520Z
 
 class Solution:
     def characterReplacement(self, s, k):
